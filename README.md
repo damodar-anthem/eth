acfab8c466818cf280b5f968265bf0b118ae56ce
Damodar Dahal <damodar.dahal@anthem.com>
2021-05-29 09:48:29 -0500

    Run lint

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # eth
+
 End-to-End Test everything using Cypress, TypeScript and more.
 
 ## Setup
-* Install `node>=16.0.0` on your system.
-* Install dependencies
+
+- Install `node>=16.0.0` on your system.
+- Install dependencies
+
 ```
 npm install
 ```
-* Run cypress
+
+- Run cypress
+
 ```
 npx cypress open
 ```
-
--- cypress/integration/weather/basic.spec.ts
+++ cypress/integration/weather/basic.spec.ts
@@ -1,23 +1,20 @@
 /// <reference types="cypress" />
 
 context('Weather', () => {
+  it('service provides weather for Hammond, LA.', () => {
+    cy.visit('https://www.weather.gov/')
 
-    it('service provides weather for Hammond, LA.', () => {
-        cy.visit("https://www.weather.gov/");
+    cy.get('#inputstring').type('Hammond, LA')
 
-        cy.get("#inputstring")
-            .type("Hammond, LA");
-        
-        cy.get(".autocomplete-suggestion")
-            .first()
-            .contains('Hammond, LA, USA')
-            .click()
-        
-        cy.url()
-            .should("contain", "https://forecast.weather.gov/MapClick.php")
-        
-        cy.get("#current-conditions .panel-title")
-            .contains("Hammond, Hammond Municipal Airport")
-        
-    })
+    cy.get('.autocomplete-suggestion')
+      .first()
+      .contains('Hammond, LA, USA')
+      .click()
+
+    cy.url().should('contain', 'https://forecast.weather.gov/MapClick.php')
+
+    cy.get('#current-conditions .panel-title').contains(
+      'Hammond, Hammond Municipal Airport',
+    )
+  })
 })
